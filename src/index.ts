381691c793fe824ad6138c33996594b236fc89b0
vapvin <vins@vins.dev>
2020-12-14 01:29:47 +0900

    Setting Base Project

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello World");
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        dbName: 'ladit',
+        user: '',
+        password: '',
+        type: 'postgresql',
+        debug: !__prod__,
+    });
+}
